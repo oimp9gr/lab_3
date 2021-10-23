aa6685ac79f1e7d26f97f0006dfffa67b23ab2cf
BroodingKangaroo <johngolt33@gmail.com>
2021-10-23 21:30:04 +0300

    Remove solutions

--- solutions/a5.h
+++ solutions/a5.h
@@ -1,16 +1,1 @@
 #pragma once
-
-#include <vector>
-
-using namespace std;
-
-void RotateMatrix(vector<vector<int>> &matrix) {
-    size_t n = matrix.size();
-    vector<vector<int>> result(n, vector<int>(n));
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < n; ++j) {
-            result[j][n - 1 - i] = matrix[i][j];
-        }
-    }
-    matrix = result;
-}
